35111921a50d5a1904554afa11b4a01ae65e32dc
boby bewa <bobybewa6@mail.com>
2021-08-30 09:58:49 +0700

    cors

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 3000
 const routes = require('./routes/index')
+app.use(cors())
+app.use((req,res,next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+        next()
+    })
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
-app.use(cors)
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-    next()
-})
 
 app.listen(port, () => {console.log(`listening on ${port}`)})
